5013c2767f1111e7fb5cd879ae49f2105871fd80
springdevelopmentteam <107068134+springdevelopmentteam@users.noreply.github.com>
2022-06-16 11:14:08 +0630

    Update serializers.py

--- actor/serializers.py
+++ actor/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class ActorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Actor 
-        fields = ['id', 'actor_image', 'actor_name', 'dob', 'zodic_sign', 'drama_name', 'actor_profile', 'update_datetime'] 
+        fields = ['id', 'actor_image', 'actor_name', 'surname', 'dob', 'zodic_sign', 'drama_name', 'actor_profile', 'update_datetime'] 
